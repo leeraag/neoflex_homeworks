bd4a37490f8b46591120118c2e216b5deabc3c5d
Valeriia <valeriaagapova.2001@gmail.com>
2024-05-28 20:21:49 +0300

    Task_02 form fixed regex

--- Task_02_JavaScript/form/form.js
+++ Task_02_JavaScript/form/form.js
@@ -1,17 +1,14 @@
-// Устаревшие браузеры поддерживают несколько способов получения DOM-узла
 const form = document.getElementById("form");
 const fio = document.getElementById("fio")
 const password = document.getElementById("password");
 
-// let errorFio = '';
-// let errorPassword = '';
 let errorFio = fio;
 while ((errorFio = errorFio.nextElementSibling).nodeType != 1); // ???
 
 let errorPassword = password;
 while ((errorPassword = errorPassword.nextElementSibling).nodeType != 1); // ???
 
-const fioPattern = /^[А-ЯЁа-яёA-Za-z]+(-[А-ЯЁа-яёA-Za-z]+)? [А-ЯЁа-яёA-Za-z]+( [А-ЯЁа-яёA-Za-z]+)?$/;
+const fioPattern = /^[А-ЯЁа-яёA-Za-z]{3,}([-][А-ЯЁа-яёA-Za-z]{3,})?\s[А-ЯЁа-яёA-Za-z]{3,}\s[А-ЯЁа-яёA-Za-z]{3,}$/;
 const passwordPattern = /^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[#?!@$%^&*-.]).{8,}$/;
 
 fio.addEventListener("input", function () {
@@ -46,7 +43,6 @@ form.addEventListener("submit", function (event) {
     const fioTest = fioPattern.test(fio.value);
     const passwordTest = passwordPattern.test(password.value);
     if (fioTest && passwordTest) {
-        // event.preventDefault()
         alert('Корректные данные')
     } else {
         if (!fioTest) {
@@ -68,9 +64,6 @@ form.addEventListener("submit", function (event) {
             password.className = "form__input valid";
             errorPassword.textContent = "";
             errorPassword.className = "form__input-error";
-            // alert("Данные введены корректно" )
         }
     }
-    
-
 });
