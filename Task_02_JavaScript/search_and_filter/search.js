335c63ae9c625830ed99ef87652c7a2920d97e3a
Valeriia <valeriaagapova.2001@gmail.com>
2024-05-29 11:21:38 +0300

    Task_92 search added init function

--- Task_02_JavaScript/search_and_filter/search.js
+++ Task_02_JavaScript/search_and_filter/search.js
@@ -1,25 +1,31 @@
-const input = document.getElementById("input");
+const init = () => {
+  const input = document.getElementById("input");
 
-input.addEventListener("input", function () {
-  const filter = input.value.toUpperCase();
-  const list = document.getElementById("list");
-  const listItem = list.getElementsByTagName("li");
-  const no = document.getElementById("no");
-  let foundItems = 0;
-  for (let i = 0; i < listItem.length; i++) {
-    let item = listItem[i];
-    let itemText = item.textContent || item.innerText;
-    if (itemText.toUpperCase().indexOf(filter) > -1) {
-      listItem[i].style.display = "block";
-      foundItems ++;
-    } else {
-      listItem[i].style.display = "none";
+  input.addEventListener("input", function () {
+    const filter = input.value.toUpperCase();
+    const list = document.getElementById("list");
+    const listItem = list.getElementsByTagName("li");
+    const no = document.getElementById("no");
+    let foundItems = 0;
+    for (let i = 0; i < listItem.length; i++) {
+      let item = listItem[i];
+      let itemText = item.textContent || item.innerText;
+      if (itemText.toUpperCase().indexOf(filter) > -1) {
+        listItem[i].style.display = "block";
+        foundItems ++;
+      } else {
+        listItem[i].style.display = "none";
+      }
     }
-  }
-  if (foundItems > 0) {
-    no.style.display = "none";
-  }
-  else {
-    no.style.display = "block";
-  }
-})
+    if (foundItems > 0) {
+      no.style.display = "none";
+    }
+    else {
+      no.style.display = "block";
+    }
+  })
+}
+
+document.addEventListener('DOMContentLoaded', function() {
+  init();
+});
