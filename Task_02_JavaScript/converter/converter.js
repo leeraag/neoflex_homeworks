8c1bf704960f93354517bfa54f27f4bdf94f8d08
Valeriia <valeriaagapova.2001@gmail.com>
2024-05-29 11:34:37 +0300

    Task_02 converter added init function and some fixes

--- Task_02_JavaScript/converter/converter.js
+++ Task_02_JavaScript/converter/converter.js
@@ -1,45 +1,51 @@
-const API_KEY_CURRENCY = '5f8fee5e22mshed13ce4325035a3p1ddfb1jsnc1a690fb9aa9'
-const API_HOST_CURRENCY = 'currency-exchange.p.rapidapi.com'
-const API_URL_CURRENCY = 'https://currency-exchange.p.rapidapi.com/exchange'
+const init = () => {
+    const API_KEY_CURRENCY = '5f8fee5e22mshed13ce4325035a3p1ddfb1jsnc1a690fb9aa9'
+    const API_HOST_CURRENCY = 'currency-exchange.p.rapidapi.com'
+    const API_URL_CURRENCY = 'https://currency-exchange.p.rapidapi.com/exchange'
 
-const loader = document.getElementById("loader")
-const getCurrencies = async () => {
-    const currencies = ['USD', 'EUR', 'CAD', 'CNY', 'CHF', 'SGD']
-    const countCurrency = 6
-    let currencyValues = []
-    loader.style.display = "block";
-    for (let i = 0; i < countCurrency; i++) {
-        const options = {
-            method: 'GET',
-            url: 'https://currency-exchange.p.rapidapi.com/exchange',
-            params: {
-                from: currencies[i],
-                to: 'RUB',
-                q: '1.0'
-            },
-            headers: {
-                'X-RapidAPI-Key': '5f8fee5e22mshed13ce4325035a3p1ddfb1jsnc1a690fb9aa9',
-                'X-RapidAPI-Host': 'currency-exchange.p.rapidapi.com'
-            }};
-            try {
-                const response = await axios.request(options);
-                currencyValues.push(response.data.toFixed(2))
-            } catch (error) {
-                console.error(error);
+    const loader = document.getElementById("loader")
+    const getCurrencies = async () => {
+        const currencies = ['USD', 'EUR', 'CAD', 'CNY', 'CHF', 'SGD']
+        const countCurrency = 6
+        let currencyValues = []
+        loader.style.display = "block";
+        for (let i = 0; i < countCurrency; i++) {
+            const options = {
+                method: 'GET',
+                url: API_URL_CURRENCY,
+                params: {
+                    from: currencies[i],
+                    to: 'RUB',
+                    q: '1.0'
+                },
+                headers: {
+                    'X-RapidAPI-Key': API_KEY_CURRENCY,
+                    'X-RapidAPI-Host': API_HOST_CURRENCY
+                }};
+                try {
+                    const response = await axios.request(options);
+                    currencyValues.push(response.data.toFixed(2))
+                } catch (error) {
+                    console.error(error);
+                }
             }
-        }
-    loader.style.display = "none";
-    showCurrencies(currencyValues, currencies)
+        loader.style.display = "none";
+        showCurrencies(currencyValues, currencies)
+    }
+
+    const showCurrencies = (currencyValues, currencies) => {
+        const list = document.getElementById("list");
+        let listCurrencies = ''
+        currencyValues.forEach((val, index) => {
+            listCurrencies += `<li><span>${currencies[index]}: </span> ${ val }</li>`
+        })
+        list.innerHTML = listCurrencies;
+    }
+
+    getCurrencies() // при загрузке страницы
+    setInterval(getCurrencies, 15 * 60 * 1000); // вызывается каждые 15 мин
 }
 
-const showCurrencies = (currencyValues, currencies) => {
-    const list = document.getElementById("list");
-    let listCurrencies = ''
-    currencyValues.forEach((val, index) => {
-        listCurrencies += `<li><span>${currencies[index]}: </span> ${ val }</li>`
-    })
-    list.innerHTML = listCurrencies;
-}
-
-getCurrencies() // при загрузке страницы
-setInterval(getCurrencies, 15 * 60 * 1000); // вызывается каждые 15 мин
+document.addEventListener('DOMContentLoaded', function() {
+    init();
+});
